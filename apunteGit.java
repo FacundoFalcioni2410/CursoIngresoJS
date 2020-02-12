bfb7447f7d544d2f23b938098bc073213b783c5f
FacundoFalcioni2410 <facundofalcioni2410@gmail.com>
2020-02-12 10:04:06 -0300

    Apunte de git

--- apunteGit.java
+++ apunteGit.java
@@ -2,9 +2,9 @@ git clone https://github.com/FacundoFalcioni2410/CursoIngresoJS.git
 
 git status
 
-git add .
+git add . //agrega todos los archivos
 
-git commit -m "mensaje"
+git commit -m "mensaje" //crea un commit para el archivo guardado
 
-git config --global user.email "mail"
+git config --global user.email "mail" //iniciar sesion
 git config --global user.name "nombre"
