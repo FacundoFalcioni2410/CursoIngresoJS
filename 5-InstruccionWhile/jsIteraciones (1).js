31a4826a358f887e45c6c53f85e0c1afdc3a53cc
alumno <alumno>
2020-02-18 12:02:32 -0300

    while

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,9 @@
 function mostrar()
 {
 	alert('iteración while');
-
+	/*while(   )
+	ejecuta la linea y vuelve a evaluar
+	tiene que modificar su condicion, sino se repite constantemente
+	
 
 }//FIN DE LA FUNCIÓN
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
+	var laHora;
+	
+	laHora=document.getElementById('laHora').value;
+	laHora=parseInt(laHora);
+
+	switch(laHora)
+	{
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+			alert("es de mañana");
+			break;
+		case 12:
+		case 13:
+		case 14:		
+	}
 
 }
