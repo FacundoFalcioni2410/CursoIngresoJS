f2dd355fe16a8ec0334472638cb51c7f3df1cb0e
alumno <alumno>
2020-02-18 11:17:06 -0300

    clase switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -7,38 +7,22 @@ function mostrar()
 	switch(mesDelAño)
 	{
 		case"Enero":
+		case"Marzo":
+		case"Mayo":
+		case"Julio":
+		case"Agosto":
+		case"Octubre":
+		case"Diciembre":
 			alert("Tiene 31 días");
 			break;
 		case"Febrero":
 			alert("Tiene 28 días");
 			break;
-		case"Marzo":
-			alert("Tiene 31 días");
-			break;
 		case"Abril":
-			alert("Tiene 30 días");
-			break;
-		case"Mayo":
-			alert("Tiene 31 días");
-			break;
 		case"Junio":
-			alert("Tiene 30 días");
-			break;
-		case"Julio":
-		case"Agosto":
-			alert("Tiene 31 días");
-			break;
 		case"Septiembre":
-			alert("Tiene 30 días");
-			break;
-		case"Octubre":
-			alert("Tiene 31 días");
-			break;
 		case"Noviembre":
-			alert("Tiene 30 días");
-			break;
-		case"Diciembre":
 			alert("Tiene 31 días");
-			break;								
+										
 	}
 }//FIN DE LA FUNCIÓN
