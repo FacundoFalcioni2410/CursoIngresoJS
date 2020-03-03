bebae681c66a70a5c658f32233e5d99720a4a2ee
alumno <alumno>
2020-03-03 12:10:24 -0300

    practica parcial

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,130 @@
+/*
+b)Para la gestión de un hotel,
+ingresar los siguientes datos validados de una reserva
+nombre del huésped
+cantidad de personas 
+cantidad de dia de estadia 
+forma de pago(efectivo , tarjeta o QR)
 
+informar el huésped que trajo más personas en
+una sola reserva.
+
+la cantidad de personas que se quedaron más días
+la forma de pago más utilizada.
+el promedio de cantidad de días por reserva
+*/
 function mostrar()
 {
-	alert("hola");
+	var nombre;
+	var cantidadDePersonas;
+	var cantidadDeDias;
+	var formaDePago;
+	var respuesta;
+	var maximoDePersonas;
+	var maximoDePersonasNombre;
+	var contador;
+	var acumulador;
+	var promedio;
+	var maximoDeDias;
+	var maximoDeDiasCantidad;
+	var contadorQR;
+	var contadorEfectivo;
+	var contadorTarjeta;
+	var formaDePagoMasUtilizada;
+
+	respuesta="s";
+	contador=0;
+	acumulador=0;
+	contadorQR=0;
+	contadorEfectivo=0;
+	contadorTarjeta=0;
+
+	while(respuesta=="s")
+	{
+		do
+		{
+			nombre=prompt("Ingrese nombre");
+		}
+		while(!isNaN(nombre));
+		do
+		{
+			cantidadDePersonas=prompt("Ingrese la cantidad de personas");
+			cantidadDePersonas=parseInt(cantidadDePersonas);
+		}
+		while(isNaN(cantidadDePersonas) || cantidadDePersonas<0);
+
+		console.info("Personas: ", cantidadDePersonas);
+		
+		do
+		{
+			cantidadDeDias=prompt("Ingrese la cantidad de dias");
+			cantidadDeDias=parseInt(cantidadDeDias);
+		}
+		while(isNaN(cantidadDeDias) || cantidadDeDias<0);
+
+		console.info("Dias: ", cantidadDeDias);
+		
+		do
+		{
+			formaDePago=prompt("Ingrese la forma de pago");
+		}
+		while(!isNaN(formaDePago) && formaDePago!="QR" && formaDePago!="Efectivo" && formaDePago!="Tarjeta");
+
+		console.info("Forma de pago: ", formaDePago);
+
+		if(contador==0 || maximoDePersonas<cantidadDePersonas)
+		{
+			maximoDePersonas=cantidadDePersonas;
+			maximoDePersonasNombre=nombre;
+		}
+		if(contador==0 || maximoDeDias<cantidadDeDias)
+		{
+			maximoDeDias=cantidadDeDias;
+			maximoDeDiasCantidad=cantidadDePersonas;
+		}
+		switch(formaDePago)
+		{
+			case "Efectivo":
+				contadorEfectivo++;
+				break;
+			case "Tarjeta":
+				contadorTarjeta++;
+				break;
+			case "QR":
+				contadorQR++;
+				break;		
+		}
+		if(contadorEfectivo>contadorTarjeta && contadorEfectivo>contadorQR)
+		{
+			formaDePagoMasUtilizada="Efectivo";
+		}
+		else
+		{
+			if(contadorQR>contadorTarjeta)
+			{
+				formaDePagoMasUtilizada="QR";
+			}
+			else
+			{
+				formaDePagoMasUtilizada="Tarjeta";
+			}
+		}
+
+		acumulador=acumulador+cantidadDeDias
+		contador++;
+		respuesta=prompt("¿Desea continuar");
+	}
+
+
+	promedio=acumulador/contador;
+	console.info("Maximo persona", maximoDePersonasNombre);
+	console.info("Maximo dias: ", maximoDeDiasNombre);
+	console.info("Forma de pago mas utilizada: ", formaDePagoMasUtilizada);
+
+	document.write("<br>nombre del huesped con mas invitados: "+maximoDePersonasNombre);
+	document.write("<br>nombre de maximos dias invitados: "+maximoDePersonasNombre);
+	document.write("<br>Forma de pago mas utilizada: "+formaDePagoMasUtilizada);
+	document.write("<br>El promedio es: "+promedio);
+	document.write("<br>La cantidad de personas que se quedaron mas dias: "+maximoDeDiasCantidad);
 }
+
--- 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,124 @@
 function mostrar()
 {
+	var nombre;
+	var sexo;
+	var notaDelFinal;
+	var edad;
+	var respuesta;
+	var cantidadVaronesAprobados;
+	var acumuladorNotasMasculino;
+	var acumuladorNotasFemenino;
+	var contadorMasculino;
+	var contadorFemenino;
+	var promedioNotasFinalesMasculino;
+	var promedioNotasFinalesFemenino;
+	var notasMenoresDeEdad;
+	var notasAdolescentes;
+	var notasMayoresDeEdad;
+	var cantidadMenores;
+	var cantidadAdolescentes;
+	var cantidadMayores;
+	var promedioNotasMayores;
+	var promedioNotasMenores;
+	var promedioNotasAdolescentes;
 
+	respuesta="Si"
+	cantidadVaronesAprobados=0;
+	contadorMasculino=0;
+	contadorFemenino=0;
+	acumuladorNotasMasculino=0;
+
+	while(respuesta=="Si")
+	{
+		do
+		{
+			nombre=prompt("Ingrese su nombre");
+		}
+		while(!isNaN(nombre));
+
+		do
+		{
+			edad=prompt("Ingrese su edad");
+			edad=parseInt(edad);	
+		}
+		while(isNaN(edad) || edad>99 || edad<1);
+		
+		do
+		{
+			sexo=prompt("Ingrese el sexo");
+		}
+		while(!isNaN(sexo) || sexo!="F" && sexo !="M");
+
+		do
+		{
+			notaDelFinal=prompt("Ingrese la nota del final");
+			notaDelFinal=parseInt(notaDelFinal);
+		}
+		while(isNaN(notaDelFinal) || notaDelFinal<1 || notaDelFinal>10);
+
+		switch(sexo)
+		{
+			case "M":
+				contadorMasculino++;
+				acumuladorNotasMasculino=acumuladorNotasMasculino + notaDelFinal;
+			default:
+				contadorFemenino++;
+				acumuladorNotasFemenino=acumuladorNotasFemenino + notaDelFinal;
+		}
+		
+		if(sexo=="M" && notaDelFinal>5)
+		{
+			cantidadVaronesAprobados++;
+		}
+
+		switch(edad)
+		{
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+			case 11:
+			case 12:
+				notasMenoresDeEdad=notasMenoresDeEdad+notaDelFinal;
+				cantidadMenores++;
+				break;
+			case 13:
+			case 14:
+			case 15:
+			case 16:
+			case 17:
+			case 18:
+			case 19:
+			case 20:
+			case 21:
+			case 22:
+			case 23:
+			case 24:
+			case 25:
+				notasAdolescentes=notasAdolescentes+notaDelFinal;
+				cantidadAdolescentes++;
+				break;
+			default:
+				notasMayoresDeEdad=notasMayoresDeEdad+notaDelFinal;
+				cantidadMayores++;
+		}
+		respuesta=prompt("¿Desea continuar?");
+	}
+	promedioNotasAdolescentes=notasAdolescentes/cantidadAdolescentes;
+	promedioNotasMenores=notasMenoresDeEdad/cantidadMenores;
+	promedioNotasMayores=notasMayoresDeEdad/cantidadMayores;
+	promedioNotasFinalesMasculino=acumuladorNotasMasculino/contadorMasculino;
+	promedioNotasFinalesFemenino=acumuladorNotasFemenino/contadorFemenino;
+	document.write("<br>La cantidad de varones aprobados es: "+cantidadVaronesAprobados);
+	document.write("<br>El promedio total de las notas finales de los varones es: "+promedioNotasFinalesMasculino);
+	document.write("<br>El promedio total de las notas finales de las mujeres es: "+promedioNotasFinalesFemenino);
+	document.write("<br>El promedio total de las notas finales de los adolescentes es: "+promedioNotasAdolescentes);
+	document.write("<br>El promedio total de las notas finales de los mayores es: "+promedioNotasMayores;
+	document.write("<br>El promedio total de las notas finales de las menores es: "+promedioNotasMenores);
 }
