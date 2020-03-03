14d9867df2e64b459cb608f245a5f562e6635349
FacundoFalcioni2410 <facundofalcioni2410@gmail.com>
2020-03-03 18:29:41 -0300

    practica

--- 9-Parciales/2019_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/ocho.js
@@ -1,4 +1,73 @@
+/*Se pide la carga de datos de un contenedor  de alimento canino con un máximo de carga de 1000kg, cargar hasta que el cliente quiera o se llene el contenedor.
+se pide:
+marca
+kilos por bolsa
+cantidad de bolsa
+importe por bolsa
+
+que marca tiene más kilos en el contenedor
+que marca tiene más bolsas de alimento en el contenedor
+que marca tiene el mayor importe por bolsa de alimento
+el importe y la marca de la bolsa de alimento menos pesada.*/
+
 function mostrar()
 {
+	var cantidadDeBolsas;
+	var sabor;
+	var kilos;
+	var respuesta;
+	var contador;
+	var sumaDekilos;
+	var promedioDeKilos;
+	var bandera;
+	var maximoDeKilos;
+	var maximoDeKilosSabor;
+	var contadorCarne;
+	var sumaDeKilosCarne;
+	var promedioDeKilosCarne;
 
+	contador=0;
+	bandera=0;
+
+	while
+	{
+		do
+		{
+			cantidadDeBolsas=prompt("Ingrese la cantidad de bolsas");
+			cantidadDeBolsas=parseInt(cantidadDeBolsas);
+		}
+		while(isNaN(marca) || cantidadDeBolsas<1 || cantidadDeBolsas>10);
+		do
+		{
+			kilos=prompt("Ingrese la cantidad de kilos que desea");
+			kilos=parseInt(kilos);
+		}
+		while(isNaN(kilos) || kilos<1 || kilos>500);
+		do
+		{
+			sabor=("Ingrese el sabor deseado");
+		}
+		while(!isNaN(sabor) || sabor!="Carne" && sabor!="Vegetales" && sabor!="Pollo");
+		
+		sumadekilos=kilos*cantidadDeBolsas;
+		contador++;
+		if(bandera==0 || kilos>maximoDeKilos)
+		{
+			maximoDeKilos=kilos;
+			maximoDeKilosSabor=sabor;
+		}
+		switch(sabor)
+		{
+			case "Carne":
+				contadorCarne++;
+				sumaDeKilosCarne=sumaDeKilosCarne+kilos;
+				break;
+		}
+	}
+		promedioDeKilos=sumaDeKilos/contador;
+		promedioDeKilosCarne=sumaDeKilosCarne/contadorCarne;
+
+		alert("El promedio de kilos es de: "+kilos);
+		alert("El mas liviano es de: "+kilos+ " y su sabor es: "+sabor);
+		alert("La cantidad de carne es: "+contadorCarne+" y su promedio de kilos es: "+promedioDeKilosCarne);
 }
--- 9-Parciales/2019_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,65 @@
 function mostrar()
 {
+	var cantidadDeBolsas;
+	var sabor;
+	var kilos;
+	var respuesta;
+	var contador;
+	var sumaDeKilos;
+	var promedioDeKilos;
+	var bandera;
+	var maximoDeKilos;
+	var maximoDeKilosSabor;
+	var contadorCarne;
+	var sumaDeKilosCarne;
+	var promedioDeKilosCarne;
 
+	contador=0;
+	bandera=0;
+	respuesta="Si"
+	contadorCarne=0;
+	promedioDeKilosCarne=0;
+	
+	while(respuesta=="Si")
+	{
+		do
+		{
+			cantidadDeBolsas=prompt("Ingrese la cantidad de bolsas");
+			cantidadDeBolsas=parseInt(cantidadDeBolsas);
+		}
+		while(isNaN(cantidadDeBolsas) || cantidadDeBolsas<1 || cantidadDeBolsas>10);
+		do
+		{
+			kilos=prompt("Ingrese la cantidad de kilos que desea");
+			kilos=parseInt(kilos);
+		}
+		while(isNaN(kilos) || kilos<1 || kilos>500);
+		do
+		{
+			sabor=prompt("Ingrese el sabor deseado");
+		}
+		while(!isNaN(sabor) || sabor!="Carne" && sabor!="Vegetales" && sabor!="Pollo");
+		
+		sumaDekilos=kilos*cantidadDeBolsas;
+		contador++;
+		if(bandera==0 || kilos>maximoDeKilos)
+		{
+			maximoDeKilos=kilos;
+			maximoDeKilosSabor=sabor;
+		}
+		switch(sabor)
+		{
+			case "Carne":
+				contadorCarne++;
+				sumaDeKilosCarne=sumaDeKilosCarne+kilos;
+				break;
+		}
+		respuesta=prompt("¿Desea continuar?");
+	}
+		promedioDeKilos=sumaDeKilos/contador;
+		promedioDeKilosCarne=sumaDeKilosCarne/contadorCarne;
+
+		alert("El promedio de kilos es de: "+kilos);
+		alert("El mas liviano es de: "+kilos+ " y su sabor es: "+sabor);
+		alert("La cantidad de carne es: "+contadorCarne+" y su promedio de kilos es: "+promedioDeKilosCarne);
 }
